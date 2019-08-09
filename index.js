45a877cc52ad089b7d020db36da0d2c621346101
banobepascal <banobepascal@gmail.com>
2019-08-09 14:35:58 +0300

    using fetch to post

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const app = express();
 
 app.listen(3000, () => console.log('listening on 3000'));
 app.use(express.static('public'));
+
+app.post('/api', (req, res, next) => {
+    console.log(req);
+});
