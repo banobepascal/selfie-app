d204bce4f776ac7f63a9b879b30b2b7ec5813455
banobepascal <banobepascal@gmail.com>
2019-08-10 15:37:47 +0300

    logging the request

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.listen(3000, () => console.log('listening on 3000'));
 app.use(express.static('public'));
 
 app.post('/api', (req, res, next) => {
-    con
+    console.log("I got your request");
     console.log(req.body);
 });
