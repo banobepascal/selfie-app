e61640f79eb7a3e29117b530b83896b9b61253fb
banobepascal <banobepascal@gmail.com>
2019-08-10 17:59:07 +0300

    testing database

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const Datastore = require('nedb');
 
 app.listen(3000, () => console.log('listening on 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+database.insert({ name: 'Pascal', status: 'Single'});
 
 app.post('/api', (req, res, next) => {
     console.log("I got your request");
