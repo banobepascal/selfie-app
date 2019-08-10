24678f4da48e916de7371af033501b8459d0264a
banobepascal <banobepascal@gmail.com>
2019-08-10 17:44:27 +0300

    saving to a database

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app = express();
 
 app.listen(3000, () => console.log('listening on 3000'));
 app.use(express.static('public'));
-app.use(express.json({limmit: '1mb'}));
+app.use(express.json({limit: '1mb'}));
 
 app.post('/api', (req, res, next) => {
     console.log("I got your request");
     console.log(req.body);
+    // const data = req.body;
+    res.json({
+        status: 'Success',
+        latitude: req.body.lat,
+        longitude: req.body.lon
+    });
 });
