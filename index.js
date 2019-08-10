e701ad55d64a627c2c3d108b47013f387376fd0d
banobepascal <banobepascal@gmail.com>
2019-08-10 15:41:38 +0300

    limiting data on the server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.listen(3000, () => console.log('listening on 3000'));
 app.use(express.static('public'));
+app.use(express.json({limmit: '1mb'}));
 
 app.post('/api', (req, res, next) => {
     console.log("I got your request");
